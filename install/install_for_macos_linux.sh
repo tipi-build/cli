265015cd045223a51fefcdb8f575d4b0813a46b3
Damien Buhl (alias daminetreg) <damien.buhl@lecbna.org>
2021-08-04 07:02:52 +0200

    :rocket: preparation of first tipi public release v0.0.15!

---
+++ install/install_for_macos_linux.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+if [ "$(uname)" == "Linux" ]; then
+  TIPI_URL="https://github.com/tipi-build/cli/releases/download/v0.0.15/tipi-v0.0.15-linux-x86_64.zip"
+fi
+
+if [ "$(uname)" == "Darwin" ]; then
+  TIPI_URL="https://github.com/tipi-build/cli/releases/download/v0.0.15/tipi-v0.0.15-macOS.zip"
+fi
+
+INSTALL_FOLDER="/usr/local"
+
+abort() {
+  printf " \e[91m $1 \n"
+  exit 1
+}
+
+info() {
+  printf "\e[1;32m ---> \e[0m $1 \n"
+}
+
+should_install_unzip() {
+  if [[ $(command -v unzip) ]]; then
+    return 1
+  fi
+}
+
+if should_install_unzip; then
+    info "unzip is needed to unzip the downloaded file, we are installing unzip with your package manager"
+    echo "Could you validate with your password ? 😇 "
+    sudo apt-get install unzip -y || abort "Error while installing unzip"
+fi
+
+info "Downloading tipi..."
+curl -fSL $TIPI_URL --output ~/tipi.zip || wget -q $TIPI_URL -O ~/tipi.zip || abort "Could not download tipi"
+info "Installing tipi in $INSTALL_FOLDER/bin"
+sudo unzip ~/tipi.zip -d $INSTALL_FOLDER -x LICENSE && rm ~/tipi.zip
+
+if [ $? -eq 0 ]; then
+    info "tipi successfully installed. Installing the dependencies..."
+    mkdir /tmp/install_tipi && echo "#include <iostream> int main(){return 0;}">> /tmp/install_tipi/installdeps.cpp
+    $INSTALL_FOLDER/bin/tipi /tmp/install_tipi
+    if [ $? -eq 0 ]; then
+        info "tipi and its dependencies have been successfully installed"
+    else 
+        abort "Error while installing the dependencies"
+    fi
+else
+    abort "Installation failed"
+fi
---
+++ install/install_for_windows.ps1
@@ -0,0 +1,81 @@
+$INSTALL_FOLDER="C:\ProgramData\tipi"
+$TIPI_URL="https://github.com/tipi-build/cli/releases/download/v0.0.15/tipi-v0.0.15-windows-win64.zip"
+$TIPI_EXE="$INSTALL_FOLDER\tipi.exe"
+$texte = '#include <iostream>
+int main()
+{
+std::cout<<"Welcome in tipi"<<std::endl;
+return 0;
+}'
+
+function Abort {
+    param (
+        $Message
+    )
+   Write-Output " $Message "
+}
+
+function Info {
+    param (
+        $Message
+    )
+  Write-Output "---> $Message "
+}
+
+function New-TemporaryDirectory {
+  $parent = [System.IO.Path]::GetTempPath()
+  [string] $name = [System.Guid]::NewGuid()
+  $path = (Join-Path -Path $parent -ChildPath $name)
+  New-Item -ItemType Directory -Path $path
+  return $path
+}
+
+info "Downloading tipi..."
+
+$download_dir = New-TemporaryDirectory
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$downloaded_tipi_zip = (Join-Path -Path $download_dir -ChildPath "tipi.zip")[0]
+Write-Output "Dowloading tipi archive in $downloaded_tipi_zip"
+Invoke-WebRequest -Uri $TIPI_URL -OutFile $downloaded_tipi_zip
+
+if(!$?) {
+    Abort("Could not download tipi")
+    return
+}
+
+info "Installing tipi in $INSTALL_FOLDER"
+$tipi_source_exe = $download_dir[0]
+$tipi_source_exe = "$tipi_source_exe\bin\tipi.exe"
+
+New-Item -Force -ItemType Directory -Path $INSTALL_FOLDER
+Expand-Archive -Force -path $downloaded_tipi_zip -destinationpath $download_dir[0]
+Copy-Item -Force $tipi_source_exe -Destination $TIPI_EXE
+
+if (!$?){
+   Abort("Could not install tipi")
+   return
+}
+
+[Environment]::SetEnvironmentVariable(
+    "Path",
+    [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::Machine) + ";$INSTALL_FOLDER",
+    [EnvironmentVariableTarget]::Machine)
+
+if (!$?){
+   Abort("Could not put tipi on the Path environment variable")
+   return
+}
+
+info "tipi is installed, downloading included tools."
+$installdeps_folder = New-TemporaryDirectory
+$installdeps_file = (Join-Path -Path $installdeps_folder -ChildPath "installdeps.cpp")[0]
+$text = $texte | Out-File -Encoding "ASCII" -FilePath "$installdeps_file"
+
+cmd.exe /c "$TIPI_EXE $installdeps_folder"   
+if ($?){
+    info "tipi has been installed in $INSTALL_FOLDER. In either a new cmd of after a reboot tipi will be available on your Path."
+ 
+}else{
+    Abort "Installation failed, please contact us via tipi.io. We would be happy to help you."
+    [Environment]::Exit(1)  
+}
