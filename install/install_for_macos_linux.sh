c7d3215be1cefac1552ec62f1a0001d012c93a8d
Lambourl <lmbrluc@gmail.com>
2021-10-21 18:26:59 +0200

    :wrench: delete libffi

--- install/install_for_macos_linux.sh
+++ install/install_for_macos_linux.sh
@@ -27,12 +27,11 @@ should_install_unzip() {
 }
 
 if [ -f "/etc/arch-release" ]; then
-  pacman -Sy
+  pacman -Syu --noconfirm
   pacman -S --needed --noconfirm python
   pacman -S --needed --noconfirm unzip
   pacman -S --needed --noconfirm base-devel
   pacman -S --needed --noconfirm openssh
-  pacman -S --needed --noconfirm libffi
 fi
 
 
