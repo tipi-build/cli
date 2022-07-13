15dea529146fa46c7e0740d542e015bd9c317bbb
Damien Buhl (alias daminetreg) <damien.buhl@lecbna.org>
2022-07-13 23:46:02 +0200

    :wrench: install v0.0.33 by default.

--- install/install_for_macos_linux.sh
+++ install/install_for_macos_linux.sh
@@ -32,7 +32,7 @@ should_install_unzip() {
 # impl.
 ### 
 
-VERSION="${TIPI_INSTALL_VERSION:-v0.0.32}"
+VERSION="${TIPI_INSTALL_VERSION:-v0.0.33}"
 INSTALL_FOLDER=/usr/local
 
 if [[ -z "${TIPI_INSTALL_SOURCE}" ]]; then
