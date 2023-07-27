420daad17ca01aa4f53123905cd4d5c769248be6
Lambourl <lmbrluc@gmail.com>
2023-07-27 09:43:43 +0200

    :rocket: update script

--- install/install_for_macos_linux.sh
+++ install/install_for_macos_linux.sh
@@ -32,7 +32,7 @@ should_install_unzip() {
 # impl.
 ### 
 
-VERSION="${TIPI_INSTALL_VERSION:-v0.0.48}"
+VERSION="${TIPI_INSTALL_VERSION:-v0.0.49}"
 INSTALL_FOLDER=/usr/local
 
 if [[ -z "${TIPI_INSTALL_SOURCE}" ]]; then
