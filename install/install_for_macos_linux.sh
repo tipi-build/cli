b605e47d61ffd8ab6eae4a28519ebc2a66f42238
Lambourl <65184664+Lambourl@users.noreply.github.com>
2023-09-06 17:10:47 +0200

    :wink: prepare release v0.0.51

--- install/install_for_macos_linux.sh
+++ install/install_for_macos_linux.sh
@@ -32,7 +32,7 @@ should_install_unzip() {
 # impl.
 ### 
 
-VERSION="${TIPI_INSTALL_VERSION:-v0.0.50}"
+VERSION="${TIPI_INSTALL_VERSION:-v0.0.51}"
 INSTALL_FOLDER=/usr/local
 
 if [[ -z "${TIPI_INSTALL_SOURCE}" ]]; then
