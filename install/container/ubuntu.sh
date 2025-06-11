1d32922aae5550f827becaf72781ac9f3116d5ce
Damien Buhl (alias daminetreg) <damien@tipi.build>
2025-06-11 12:02:29 +0200

    :gear: replicate official tipi Dockerfile creation logic

--- install/container/ubuntu.sh
+++ install/container/ubuntu.sh
@@ -5,6 +5,7 @@ set -e
 # tipi docker setup script for debian and arch derivativees
 # Copyright 2024 - tipi technologies Ltd (Zürich)
 
+# INCLUDE+ common/Dockerfile.apt-install-required
 apt-get -y update && apt-get install -y \
   openssh-server \
   sudo \
@@ -15,29 +16,99 @@ apt-get -y update && apt-get install -y \
   && apt-get clean \
   && rm -rf /var/lib/apt/lists/*
 
+source /etc/lsb-release
+DISTRIB_RELEASE_MAJOR=`echo $DISTRIB_RELEASE | sed 's/\([0-9]\+\)\..*/\1/'`
+
+# INCLUDE+ common/Dockerfile.apt-install-required-before-2204
+if [ ${DISTRIB_RELEASE_MAJOR} -le 20 ]; then
+  apt-get -y update && apt-get install -y \
+    python \
+    locales \
+    ca-certificates \
+    build-essential \
+    autotools-dev \
+    autoconf \
+    m4 \
+    libtool \
+    gcovr \
+    iptables \
+    apt-utils \
+    pkg-config \
+    automake \
+    autoconf-archive \
+    autopoint \
+    flex \
+    libcap-dev \
+    libssl-dev \
+    zlib1g-dev \
+    byacc \
+    bison \
+    freeglut3-dev \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+
+  locale-gen "en_US.UTF-8"
+fi
+
+# INCLUDE+ common/Dockerfile.update-su
+if [ ${DISTRIB_RELEASE_MAJOR} -le 16 ]; then
+  apt-get -y update && apt-get install -y \
+    wget \
+    libpam-dev \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+
+  locale-gen "en_US.UTF-8"
+
+  wget https://mirrors.edge.kernel.org/pub/linux/utils/util-linux/v2.39/util-linux-2.39.tar.gz  \
+    && tar xvzf util-linux-2.39.tar.gz  \
+    && cd util-linux-2.39/  \
+    && ./configure \
+    && make su \
+    && cd .. \
+    && mv util-linux-2.39/su /bin/su \
+    && chmod u+s /bin/su \
+    && rm -rf util-linux-2.39/ \
+    && rm -rf util-linux-2.39.tar.gz 
+
+  # X3 Root CA Certificate Deprecation Bug
+  sed -i 's#mozilla/DST_Root_CA_X3.crt#!mozilla/DST_Root_CA_X3.crt#' /etc/ca-certificates.conf && update-ca-certificates
+fi
+
+# INCLUDE+ common/Dockerfile.apt-install-required-2204
+if [ ${DISTRIB_RELEASE_MAJOR} -ge 20 ]; then
+  apt-get -y update && apt-get install -y \
+    python3 \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+fi
+
+
+# INCLUDE+ common/Dockerfile.remote-build-ssh-access
 # Enable login with tipi generated ssh-rsa on Ubuntu 22 and onwards, use PubkeyAcceptedKeyTypes vs PubkeyAcceptedAlgorithms as it works on ubuntu 20.04 as well.
 mkdir -p /etc/ssh/sshd_config.d/ && \
     printf "\nPubkeyAcceptedKeyTypes +ssh-rsa\n" > /etc/ssh/sshd_config.d/add-ssh-rsa.conf
 service ssh start # Create /run/sshd privilege separation directory, the docker CMD should still be something like `/usr/sbin/sshd -D -o ListenAddress=0.0.0.0 -e;`
 
+# INCLUDE+ common/Dockerfile.tipi-non-root-user
 # User id 1001 is the user on Github Default Runner and 123 is the group of the files on the Github Default Runner
-addgroup --gid 123 gh-actions-group
-addgroup --gid 124 wine
+groupadd --gid 123 gh-actions-group
+groupadd --gid 124 wine
 
 if [ ! $(getent group 1001) ]; then
-  addgroup --gid 1001 tipi
+  groupadd --gid 1001 tipi
 fi
 useradd --system --uid 1001 --gid 1001 -G gh-actions-group,wine,sudo --create-home --home-dir /home/tipi tipi \
   && echo 'tipi ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/tipi
 
 if [ ! $(getent group 1000) ]; then
-  addgroup --gid 1000 tipi-large
+  groupadd --gid 1000 tipi-large
 fi
 useradd --system --uid 1000 --gid 1000 -G gh-actions-group,wine,sudo --create-home --home-dir /home/tipi-large tipi-large \
   && echo 'tipi-large ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/tipi-large
 
 if [ ! $(getent group 108) ]; then
-  addgroup --gid 108 tipi-rbe
+  groupadd --gid 108 tipi-rbe
 fi
 # https://docs.engflow.com/re/client/bazel-first-time.html#1-prepare-a-docker-container-image-for-remote-actions
 useradd --system --uid 108 --gid 108 -G gh-actions-group,wine,sudo --create-home --home-dir /home/tipi-rbe tipi-rbe \
@@ -69,6 +140,11 @@ chsh -s /bin/bash tipi-rbe
 # This is required because of docker virtiofs on docker on macOS. ( VirtioFS is not handling permissions as expected. All mount permissions are owned by root regardless of chown : https://github.com/docker/for-mac/issues/6243 )
 git config --system --add safe.directory "*"
 
+# INCLUDE+ common/Dockerfile.rustup
+su tipi -w TIPI_INSTALL_SOURCE,TIPI_DISTRO_MODE -c "cd ~ && curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -o rustup.sh && sh rustup.sh -v -y --no-modify-path"
+su tipi -w TIPI_INSTALL_SOURCE,TIPI_DISTRO_MODE -c "/home/tipi/.cargo/bin/rustup default stable"
+
+# INCLUDE+ common/Dockerfile.install-tipi
 export TIPI_DISTRO_MODE=${TIPI_DISTRO_MODE:light}
 su tipi -w TIPI_INSTALL_SOURCE,TIPI_DISTRO_MODE -c "cd ~ && curl -fsSL https://raw.githubusercontent.com/tipi-build/cli/feature/release-v0.0.72/install/install_for_macos_linux.sh -o install_for_macos_linux.sh && /bin/bash install_for_macos_linux.sh"
 
